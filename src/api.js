ed9cc23e3c783fd88bf2ce5b5ae3f95c1c15018c
Krijesh Karki <161559557+karkik01@users.noreply.github.com>
2025-11-14 18:45:51 +1300

    Configured API connection + added .env

--- src/api.js
+++ src/api.js
@@ -1,7 +1,38 @@
+// src/api.js
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+const API_URL = process.env.REACT_APP_API_URL;
+
+// Axios instance
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export default API;
+// -------- AUTH ----------
+export const loginUser = async (username, password) => {
+  const response = await api.post("/api/token/", {
+    username,
+    password,
+  });
+  return response.data; // { access, refresh }
+};
+
+export const refreshToken = async (refresh) => {
+  const response = await api.post("/api/token/refresh/", { refresh });
+  return response.data;
+};
+
+// -------- ROOMS ----------
+export const getRooms = async () => {
+  const response = await api.get("/api/rooms/");
+  return response.data;
+};
+
+// -------- RESERVATIONS ----------
+export const getReservations = async () => {
+  const response = await api.get("/api/reservations/");
+  return response.data;
+};
